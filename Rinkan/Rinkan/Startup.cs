ba8fdc4ed4e2a77f2d52c1f78436e63391d7c31c
よしのぶ <@-PC>
2017-01-12 22:13:07 +0900

    te

---
+++ Rinkan/Rinkan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rinkan.Startup))]
+namespace Rinkan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
