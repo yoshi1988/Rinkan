60b428240943886e69aa39b9918d1b9dd84e22ed
よしのぶ <@-PC>
2017-01-17 00:02:46 +0900

    愛してる

--- Rinkan/Rinkan/Controllers/HomeController.cs
+++ Rinkan/Rinkan/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Rinkan.Models;
+using System.Data.Entity;
+
 
 namespace Rinkan.Controllers
 {
     public class HomeController : Controller
     {
+
+        private Rinkan.Models.Posts postBlob = new Posts();
+        private masterEntities2 masterEntity = new masterEntities2();
+
         public ActionResult Index()
         {
-            return View();
+            DbSet<Posts> list = this.masterEntity.Posts.OrderByDescending<>
+               return View(list.ToList());
         }
 
         public ActionResult About()
@@ -33,5 +41,10 @@ namespace Rinkan.Controllers
 
             return View();
         }
+
+        //public ActionResult Update(int? id, string title, string body, DateTime datetime, string tags)
+        // {
+
+        //}
     }
 }
