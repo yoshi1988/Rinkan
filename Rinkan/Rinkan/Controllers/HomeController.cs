b6be1047794150d7a2af620482e974a096cd4159
よしのぶ <@-PC>
2017-01-18 23:49:09 +0900

    blob system added

--- Rinkan/Rinkan/Controllers/HomeController.cs
+++ Rinkan/Rinkan/Controllers/HomeController.cs
@@ -11,14 +11,22 @@ namespace Rinkan.Controllers
 {
     public class HomeController : Controller
     {
-
         private Rinkan.Models.Posts postBlob = new Posts();
         private masterEntities2 masterEntity = new masterEntities2();
 
-        public ActionResult Index()
+        private const int PostsPerpage = 5;
+
+        public ActionResult Index(int? id)
         {
-            DbSet<Posts> list = this.masterEntity.Posts.OrderByDescending<>
-               return View(list.ToList());
+            int pageNumber = id ?? 0;
+            //IEnumerable<Posts> posts = this.masterEntity.Posts.Where(x => x.Datetime < DateTime.Now).Skip(pageNumber * PostsPerpage).Take(PostsPerpage + 1);
+            IEnumerable<Posts> posts = this.masterEntity.Posts.Where(x => x.Datetime < DateTime.Now).OrderBy(x => x.Datetime).Skip(pageNumber * PostsPerpage).Take(PostsPerpage + 1);
+
+            ViewBag.IsPreviousLinkVisible = pageNumber > 0;
+            ViewBag.IsNextLinkVisible = posts.Count() > PostsPerpage;
+            ViewBag.PageNumber = pageNumber;
+
+            return View(posts.Take(PostsPerpage));
         }
 
         public ActionResult About()
@@ -42,9 +50,61 @@ namespace Rinkan.Controllers
             return View();
         }
 
-        //public ActionResult Update(int? id, string title, string body, DateTime datetime, string tags)
-        // {
+        public ActionResult Update(int? id, string title, string body, DateTime datetime, string tags)
+        {
+            if (!isAdmin)
+            {
+                return RedirectToAction("Index");
+            }
 
-        //}
+            Posts post = GetPost(id);
+            post.Title = title;
+            post.Body = body;
+            post.Datetime = datetime;
+
+
+            //            tags = tags ?? string.Empty;
+            //            string[] tagNames = tags.Split(new char[] { '' }, StringSplitOptions.RemoveEmptyEntries);
+            // foreach (string tagName in tagNames)
+            // {
+            //TODO Tagシステムの追加
+            // post.PostsTags.Tags.PostsTags(GET)
+
+            //post.po
+            //   this.masterEntity.Tags(GetTag(tagName));
+            //}
+
+            if (!id.HasValue)
+            {
+                this.masterEntity.Posts.Add(post);
+            }
+            this.masterEntity.SaveChanges();
+
+            return View("Details", new { id = post.ID});
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Posts post = GetPost(id);
+            //foreach(PostsTags postTag in post.PostsTags)
+            //{
+
+            //        }
+            ViewBag.Tags = "TODO";
+            return View(post);
+        }
+
+
+        private Tags GetTag(string tagName)
+        {
+            return this.masterEntity.Tags.Where(x => x.Name == tagName).FirstOrDefault() ?? new Tags() { Name = tagName }; 
+        }
+
+        private Posts GetPost(int? id)
+        {
+            return id.HasValue ?  this.masterEntity.Posts.Where(x => x.ID == id).First() : new Rinkan.Models.Posts () { ID = -1 };
+        }
+
+        public bool isAdmin { get { return true; } }
     }
 }
